99f49607c5f5c902f1123d7d68479139fb7ed553
nghiant <nghiant9608@gmail.com>
2025-09-10 15:59:18 +0700

    config fe

--- sms-marketing.fe/src/environments/environment.ts
+++ sms-marketing.fe/src/environments/environment.ts
@@ -2,7 +2,7 @@ import { IEnvironment } from "@/shared/models/environment.models";
 
 export const environment: IEnvironment = {
     production: true,
-    baseUrl: 'https://smsapi.huce.edu.vn/',
+    baseUrl: 'https://smsapi.huce.edu.vn',
     authGrantType: 'password',
     authClientId: 'client-web',
     authClientSecret: 'mBSQUHmZ4be5bQYfhwS7hjJZ2zFOCU2e',
