baff7e892552c5320a3a44183e0ab4e506ea964d
kiinp9 <vietcuong23122k2@gmail.com>
2025-09-10 16:21:50 +0700

    update sinh qr

---
+++ thongbao.be.infrastructure.external/Log/Logs.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace thongbao.be.infrastructure.external.Log
+{
+    internal class Logs
+    {
+    }
+}
