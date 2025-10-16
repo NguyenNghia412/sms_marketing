89b87abe2d4ac2ec3596f368b667fff309ddda7b
Kiin <vietcuong23122k2@gmail.com>
2025-10-16 16:17:32 +0700

    fix bug

--- sms-marketing.fe/src/app/pages/notfound/notfound.ts
+++ sms-marketing.fe/src/app/pages/notfound/notfound.ts
@@ -29,10 +29,14 @@ import { AppFloatingConfigurator } from '../../layout/component/app.floatingconf
                 </svg>
                 <div style="border-radius: 56px; padding: 0.3rem; background: linear-gradient(180deg, color-mix(in srgb, var(--primary-color), transparent 60%) 10%, var(--surface-ground) 30%)">
                     <div class="w-full bg-surface-0 dark:bg-surface-900 py-20 px-8 sm:px-20 flex flex-col items-center" style="border-radius: 53px">
-                        <span class="text-primary font-bold text-3xl">404</span>
-                        <h1 class="text-surface-900 dark:text-surface-0 font-bold text-3xl lg:text-5xl mb-2">Not Found</h1>
-                        <div class="text-surface-600 dark:text-surface-200 mb-8">Tính năng đang được nâng cấp. Vui lòng liên hệ Admin để nhận thêm thông tin chi tiết.</div>
-                        <a routerLink="/" class="w-full flex items-center py-8 border-surface-300 dark:border-surface-500 border-b">
+                        <!-- <span class="text-primary font-bold text-3xl">404</span> -->
+                        <!-- <h1 class="text-surface-900 dark:text-surface-0 font-bold text-3xl lg:text-5xl mb-2">Not Found</h1> -->
+                        <!-- <div class="text-surface-600 dark:text-surface-200 mb-8">Tính năng đang được nâng cấp. Vui lòng liên hệ Admin để nhận thêm thông tin chi tiết.</div> -->
+                        <h1 class="text-surface-900 dark:text-surface-0 font-bold text-xl lg:text-2xl mb-8 text-center leading-relaxed">
+                            TÀI KHOẢN CỦA BẠN CHƯA ĐƯỢC PHÂN QUYỀN,<br/>
+                            LIÊN HỆ TTCNTT ĐỂ ĐƯỢC CẬP NHẬT
+                        </h1>
+                        <!-- <a routerLink="/" class="w-full flex items-center py-8 border-surface-300 dark:border-surface-500 border-b">
                             <span class="flex justify-center items-center border-2 border-primary text-primary rounded-border" style="height: 3.5rem; width: 3.5rem">
                                 <i class="pi pi-fw pi-table text-2xl!"></i>
                             </span>
@@ -58,7 +62,7 @@ import { AppFloatingConfigurator } from '../../layout/component/app.floatingconf
                                 <span class="text-surface-900 dark:text-surface-0 lg:text-xl font-medium mb-0">Permission Manager</span>
                                 <span class="text-surface-600 dark:text-surface-200 lg:text-xl">Accumsan in nisl nisi scelerisque</span>
                             </span>
-                        </a>
+                        </a> -->
                         <p-button label="Go to Dashboard" routerLink="/" />
                     </div>
                 </div>
