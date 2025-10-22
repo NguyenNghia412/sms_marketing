ec252ab4143b39a93b0a16c6320182c858acecf4
nghiant <nghiant9608@gmail.com>
2025-10-22 09:40:46 +0700

    config build staging

--- sms-marketing.fe/src/environments/environment.staging.ts
+++ sms-marketing.fe/src/environments/environment.staging.ts
@@ -2,10 +2,10 @@ import { IEnvironment } from "@/shared/models/environment.models";
 
 export const environment: IEnvironment = {
     production: false,
-    baseUrl: 'http://localhost:5069/',
+    baseUrl: 'https://smsapidev.huce.edu.vn',
     authGrantType: 'password',
     authClientId: 'client-web',
     authClientSecret: 'mBSQUHmZ4be5bQYfhwS7hjJZ2zFOCU2e',
     authScope: 'openid offline_access',
-    appUrl: 'http://localhost:4200',
+    appUrl: 'https://smsdev.huce.edu.vn',
 };
