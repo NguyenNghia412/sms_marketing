9ec85387cf7b51e53137d06d6256c43c177e953c
Kiin <vietcuong23122k2@gmail.com>
2025-10-29 21:08:56 +0700

    log bug api profile stringee

--- thongbao.be.lib/Stringee/Implements/ProfileService.cs
+++ thongbao.be.lib/Stringee/Implements/ProfileService.cs
@@ -39,49 +39,36 @@ namespace thongbao.be.lib.Stringee.Implements
             _baseUrl = _configuration["Stringee:ProfileBaseUrl"] ?? "";
             _exchangeApiUrl = _configuration["ExchangeApi:Url"] ?? "";
         }
-
         public async Task<BaseResponseProfile?> GetProfileStringeeInfor()
         {
+
             var isSuperAdmin = IsSuperAdmin();
             var jwtToken = await _authService.GenerateAccountJwtTokenAsync();
-
-            _logger.LogInformation($"[GetProfileStringeeInfor] START - IsSuperAdmin={isSuperAdmin}, URL={_baseUrl}");
-            _logger.LogInformation($"[GetProfileStringeeInfor] Token Generated: {jwtToken}");
-
+            // var httpContent = new StringContent("application/json");
             using var httpClient = new HttpClient();
             httpClient.DefaultRequestHeaders.Add("X-STRINGEE-AUTH", jwtToken);
-
-            _logger.LogInformation($"[GetProfileStringeeInfor] Sending request to Stringee API...");
-
             var response = await httpClient.GetAsync(_baseUrl);
-
-            _logger.LogInformation($"[GetProfileStringeeInfor] Response received: StatusCode={response.StatusCode}");
-
             if (!response.IsSuccessStatusCode)
             {
                 var errorContent = await response.Content.ReadAsStringAsync();
-                _logger.LogError($"[GetProfileStringeeInfor] FAILED - StatusCode={response.StatusCode}, ErrorResponse={errorContent}");
+                _logger.LogError($"Stringee API Error: StatusCode={response.StatusCode}, Response={errorContent}, URL={_baseUrl}, Token={jwtToken.Substring(0, Math.Min(20, jwtToken.Length))}...");
                 throw new UserFriendlyException(ErrorCodes.InternalServerError);
             }
-
             var responseContent = await response.Content.ReadAsStringAsync();
-            _logger.LogInformation($"[GetProfileStringeeInfor] Success Response: {responseContent}");
-
             var jsonResponse = JsonSerializer.Deserialize<JsonElement>(responseContent);
             var account = jsonResponse.GetProperty("account");
             var amountUsd = decimal.Parse(account.GetProperty("amount").GetString() ?? "0",
                            CultureInfo.InvariantCulture);
 
+
             if (isSuperAdmin)
             {
                 var vndRate = await GetExchangeRate();
                 var amount = amountUsd * Convert.ToDecimal(vndRate.ExchangeRate);
-
-                _logger.LogInformation($"[GetProfileStringeeInfor] Calculated Amount: USD={amountUsd}, VND={amount}, Rate={vndRate.ExchangeRate}");
-
                 return new BaseResponseProfile
                 {
                     Code = jsonResponse.GetProperty("r").GetInt32(),
+                    //Message = response.Message,
                     Data = new Account
                     {
                         Id = account.GetProperty("id").GetInt32(),
@@ -96,42 +83,27 @@ namespace thongbao.be.lib.Stringee.Implements
             }
             else
             {
-                _logger.LogWarning($"[GetProfileStringeeInfor] User is not SuperAdmin, returning null");
                 return null;
             }
         }
-
         public async Task<ResponseGetExchangeApiDto?> GetExchangeRate()
         {
             var isSuperAdmin = IsSuperAdmin();
-
-            _logger.LogInformation($"[GetExchangeRate] START - IsSuperAdmin={isSuperAdmin}, URL={_exchangeApiUrl}");
-
             using var httpClient = new HttpClient();
-
             var response = await httpClient.GetAsync(_exchangeApiUrl);
-
-            _logger.LogInformation($"[GetExchangeRate] Response received: StatusCode={response.StatusCode}");
-
             if (!response.IsSuccessStatusCode)
             {
                 var errorContent = await response.Content.ReadAsStringAsync();
-                _logger.LogError($"[GetExchangeRate] FAILED - StatusCode={response.StatusCode}, ErrorResponse={errorContent}");
+                _logger.LogError($"Exchange API Error: StatusCode={response.StatusCode}, Response={errorContent}, URL={_exchangeApiUrl}");
                 throw new UserFriendlyException(ErrorCodes.InternalServerError);
             }
-
             var responseContent = await response.Content.ReadAsStringAsync();
-            _logger.LogInformation($"[GetExchangeRate] Success Response: {responseContent}");
-
             var jsonResponse = JsonSerializer.Deserialize<JsonElement>(responseContent);
             var date = jsonResponse.GetProperty("date").GetDateTime();
             var exchangeRate = jsonResponse.GetProperty("usd");
             var vndRate = exchangeRate.GetProperty("vnd").GetDouble();
-
             if (isSuperAdmin)
             {
-                _logger.LogInformation($"[GetExchangeRate] SUCCESS - Date={date}, VNDRate={vndRate}");
-
                 return new ResponseGetExchangeApiDto
                 {
                     Date = date,
@@ -140,9 +112,9 @@ namespace thongbao.be.lib.Stringee.Implements
             }
             else
             {
-                _logger.LogWarning($"[GetExchangeRate] User is not SuperAdmin, returning null");
                 return null;
             }
+
         }
     }
 }
