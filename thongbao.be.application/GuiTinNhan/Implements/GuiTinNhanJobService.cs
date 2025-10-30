55266ac0e5c73c1a663970d73300bfcfef490da5
Kiin <vietcuong23122k2@gmail.com>
2025-10-30 11:52:51 +0700

    log bug stringee

--- thongbao.be.application/GuiTinNhan/Implements/GuiTinNhanJobService.cs
+++ thongbao.be.application/GuiTinNhan/Implements/GuiTinNhanJobService.cs
@@ -34,7 +34,7 @@ namespace thongbao.be.application.GuiTinNhan.Implements
         private readonly IProfileService _profileService;
         private readonly ISendSmsService _sendSmsService;
         private readonly UserManager<AppUser> _userManager;
-        private const int BATCH_SIZE = 400;
+        private const int BATCH_SIZE = 50;
         private static readonly TimeZoneInfo VietnamTimeZone = TimeZoneInfo.FindSystemTimeZoneById("SE Asia Standard Time");
 
         public GuiTinNhanJobService(
