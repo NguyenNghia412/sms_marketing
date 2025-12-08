a0c142ead0aae92b34fdd403097e7840eb4fd9cd
kiinp9 <vietcuong23122k2@gmail.com>
2025-12-08 10:28:48 +0700

    chore: fix createdby log send sms

--- thongbao.be.application/GuiTinNhan/Interfaces/IGuiTinNhanJobService.cs
+++ thongbao.be.application/GuiTinNhan/Interfaces/IGuiTinNhanJobService.cs
@@ -9,8 +9,8 @@ namespace thongbao.be.application.GuiTinNhan.Interfaces
 {
     public  interface IGuiTinNhanJobService
     {
-        public  Task ProcessGuiTinNhanBackground(int idChienDich, int? idDanhBa, List<ListSoDienThoaiDto> danhSachSoDienThoai, int idBrandName, bool IsFlashSms, bool IsAccented, string noiDung);
-        public Task SendSmsLog(object smsResponse, int idChienDich, int? idDanhBa, List<ListSoDienThoaiDto> danhSachSoDienThoai, int idBrandName, bool isAccented, string noiDung);
+        public  Task ProcessGuiTinNhanBackground(int idChienDich, int? idDanhBa, List<ListSoDienThoaiDto> danhSachSoDienThoai, int idBrandName, bool IsFlashSms, bool IsAccented, string noiDung, string currentUserId, bool isSuperAdmin);
+        public Task SendSmsLog(object smsResponse, int idChienDich, int? idDanhBa, List<ListSoDienThoaiDto> danhSachSoDienThoai, int idBrandName, bool isAccented, string noiDung, string currentUserId, bool isSuperAdmin);
         
     }
 }
