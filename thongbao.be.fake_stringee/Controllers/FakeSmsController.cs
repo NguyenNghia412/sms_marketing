694bafaf5089c8cc09bc38fc72fd11b8e899c708
nghiant <nghiant9608@gmail.com>
2025-12-10 21:14:27 +0700

    fake sms

--- thongbao.be.fake_stringee/Controllers/FakeSmsController.cs
+++ thongbao.be.fake_stringee/Controllers/FakeSmsController.cs
@@ -10,13 +10,12 @@ namespace thongbao.be.fake_stringee.Controllers
         [HttpPost]
         public async Task<IActionResult> SendSmsFake([FromBody] object dto)
         {
-            await Task.Delay(60000); // 60 seconds
-
             Random rnd = new Random();
             int number = rnd.Next(1, 11);
 
             if (number <= 3)
             {
+                await Task.Delay(30000); // 60 seconds
                 return StatusCode(StatusCodes.Status504GatewayTimeout, "The server timed out while processing the request.");
             }
 
